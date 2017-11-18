a5dfc8d5792e8b01f5e04ce7d48cd16b0c6c24fc
Peter Hill <zed.three@gmail.com>
2017-11-18 15:53:05 +0000

    Working proof-of-concept!

--- mvce_printer.py
+++ mvce_printer.py
@@ -1,50 +1,58 @@
 import re
 
-class MVCEPrinter:
+def get_dynamic_type(val):
+
+    real_type = None
+    print("vptr at : {:#x}".format(int(val['_vptr'])))
+    symbol = gdb.execute("info symbol {:#x}".format(int(val['_vptr'])),
+                      True, True)
+    vptr_symbol = symbol.split()[0]
+    print("vptr symbol is ", vptr_symbol)
+    module_vtab = re.compile(r"__(?P<module>[a-z].*)"
+                             r"_MOD___vtab_(?P=module)_"
+                             r"(?P<type>[A-Z].*)")
+    program_vtab = re.compile(r"__vtab_[a-z0-9_]*_"
+                              r"(P<type>[A-Z].*)\.[0-9]*")
+
+    if vptr_symbol.startswith("__vtab_"):
+        print("type defined in a program")
+        vtab_regex = program_vtab
+    else:
+        print("type defined in a module")
+        vtab_regex = module_vtab
+
+    type_ = re.match(vtab_regex, vptr_symbol)
+    print(type_)
+    if type_:
+        real_type = type_.group("type")
+        print("dynamic type is ", real_type)
+
+    return real_type
+
+class FortranType:
     def __init__(self, val):
         self.val = val
 
     def to_string(self):
-
-        fields = self.val.type.fields()
-        real_type = None
-        if fields is not None:
-            field_names = [field.name for field in fields]
-            if "_vptr" in field_names:
-                print("vptr at : {:#x}".format(int(self.val['_vptr'])))
-                symbol = gdb.execute("info symbol {:#x}".format(int(self.val['_vptr'])),
-                                  True, True)
-                vptr_symbol = symbol.split()[0]
-                print("vptr symbol is ", vptr_symbol)
-                module_vtab = re.compile(r"__(?P<module>[a-z].*)"
-                                         r"_MOD___vtab_(?P=module)_"
-                                         r"(?P<type>[A-Z].*)")
-                program_vtab = re.compile(r"__vtab_[a-z0-9_]*_"
-                                          r"(P<type>[A-Z].*)\.[0-9]*")
-
-                if vptr_symbol.startswith("__vtab_"):
-                    print("type defined in a program")
-                    vtab_regex = program_vtab
-                else:
-                    print("type defined in a module")
-                    vtab_regex = module_vtab
-
-                type_ = re.match(vtab_regex, vptr_symbol)
-                print(type_)
-                if type_:
-                    real_type = type_.group("type")
-                    print("dynamic type is ", real_type)
-
-        if real_type:
-            message = real_type + "(..."
-        else:
-            message = str(self.val.type) + "(..."
-
-        return message + ")"
+        return str(self.val)
 
 def my_lookup_type(val):
-    if "my" in str(val.type).lower():
-        return MVCEPrinter(val)
-    return None
+    if "Type " not in str(val.type):
+        return None
+    fields = val.type.fields()
+    if fields is not None:
+        field_names = [field.name for field in fields]
+        if "_vptr" in field_names:
+            real_type = get_dynamic_type(val)
+            print(field_names)
+            data_address = val['_data']
+            print(data_address)
+            cast_string = "*({type}*)({address:#x})".format(
+                type=real_type, address=int(val['_data']))
+            print(cast_string)
+            real_val = gdb.parse_and_eval(cast_string)
+            return FortranType(real_val)
+    else:
+        return None
 
 gdb.pretty_printers.append(my_lookup_type)
